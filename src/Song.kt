d39ab9d48a434f51a605711b2145ce733c6857bd
Mason Trimbell <mtmaster3@gmail.com>
2019-07-24 14:11:58 -0500

    Rearrange functions

--- src/Song.kt
+++ src/Song.kt
@@ -1,12 +1,3 @@
-fun main() {
-    val song1 = Song("Song1", "Artist1")
-    val song2 = Song("Song2", "Artist2")
-    val song3 = Song("Song3", "Artist3")
-
-    song2.play()
-    song2.stop()
-}
-
 class Song(val title: String, val artist: String) {
     fun play() {
         println("Playing $title by $artist")
@@ -14,4 +5,13 @@ class Song(val title: String, val artist: String) {
     fun stop() {
         println("Stopping $title by $artist")
     }
+}
+
+fun main() {
+    val song1 = Song("Song1", "Artist1")
+    val song2 = Song("Song2", "Artist2")
+    val song3 = Song("Song3", "Artist3")
+
+    song2.play()
+    song2.stop()
 }
